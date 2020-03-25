21abe543518f023dd5598b5518692700cabe9439
Polina <polinaucc@gmail.com>
2020-03-25 21:06:02 +0200

    Added main page with the ability to register and login for an unauthorized user

--- src/main/java/ua/polina/hotel_reservation/controller/AuthController.java
+++ src/main/java/ua/polina/hotel_reservation/controller/AuthController.java
@@ -67,4 +67,9 @@ public class AuthController {
         }
         return "index";
     }
+
+    @GetMapping("/index")
+    public String getIndexPage(Model model){
+        return "index";
+    }
 }
