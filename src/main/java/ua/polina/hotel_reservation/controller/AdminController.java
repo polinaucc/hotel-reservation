63c01ae2944d506cb4cb59aeeffb9d4940b3786d
Polina <polinaucc@gmail.com>
2020-06-15 20:04:55 +0300

    added JavaDoc

--- src/main/java/ua/polina/hotel_reservation/controller/AdminController.java
+++ src/main/java/ua/polina/hotel_reservation/controller/AdminController.java
@@ -22,17 +22,43 @@ import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+/**
+ * Admin controller. This controller is used for admin functions
+ */
 @Controller
 @SessionAttributes("request")
 @RequestMapping("/admin")
 public class AdminController {
+    /**
+     * The Description service.
+     */
     DescriptionService descriptionService;
+    /**
+     * The Request service.
+     */
     RequestService requestService;
+    /**
+     * The Room service.
+     */
     RoomService roomService;
+    /**
+     * The Reservation service.
+     */
     ReservationService reservationService;
+    /**
+     * Logger
+     */
     private static final Logger LOGGER = LogManager.getLogger(AdminController.class);
     private ResourceBundle rb;
 
+    /**
+     * Constructor
+     *
+     * @param descriptionService the description service
+     * @param requestService     the request service
+     * @param roomService        the room service
+     * @param reservationService the reservation service
+     */
     @Autowired
     public AdminController(DescriptionService descriptionService,
                            RequestService requestService,
@@ -45,6 +71,13 @@ public class AdminController {
         this.reservationService = reservationService;
     }
 
+
+    /**
+     * Gets the page with form for adding description.
+     *
+     * @param model the model
+     * @return the page
+     */
     @RequestMapping("/add-description")
     public String getDescriptionForm(Model model) {
         model.addAttribute("description", new DescriptionDto());
@@ -56,17 +89,32 @@ public class AdminController {
         return "admin/description-form";
     }
 
+    /**
+     * Method is used to save description
+     *
+     * @param descriptionDto the description dto
+     * @param model          the model
+     * @param bindingResult  the binding result
+     * @return the string
+     */
     @PostMapping("/add-description")
     public String addDescription(@ModelAttribute("description") DescriptionDto descriptionDto, Model model,
                                  BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "redirect:/admin/index";
         }
-        System.out.println(descriptionDto);
         descriptionService.saveNewDescription(descriptionDto);
         return "admin/description-form";
     }
 
+    /**
+     * Gets requests page.
+     *
+     * @param model the model
+     * @param page  the page
+     * @param size  the size
+     * @return the requests page
+     */
     @GetMapping("/requests")
     public String getRequestsPage(Model model,
                                   @RequestParam("page") Optional<Integer> page,
@@ -90,6 +138,13 @@ public class AdminController {
         return "admin/requests-page";
     }
 
+    /**
+     * Method is used to get rooms for page with find room form
+     *
+     * @param requestId the request id
+     * @param model     the model
+     * @return page
+     */
     @RequestMapping("/find-room/{id}")
     public String findRoom(@PathVariable("id") Long requestId, Model model) {
         Request request = requestService.getRequestById(requestId)
@@ -118,19 +173,36 @@ public class AdminController {
         return "admin/find-room";
     }
 
+    /**
+     * Method is used to find rooms that are booked on the dates desired by client
+     *
+     * @param request      the request
+     * @param wrongRooms   the list that contains reserved rooms
+     * @param reservations the reservations
+     * @param room         current room for checking
+     */
     private void findWrongRooms(Request request, List<Room> wrongRooms, List<Reservation> reservations, Room room) {
         for (Reservation res : reservations) {
             if (res.getRoom().equals(room) &&
                     ((request.getCheckInDate().compareTo(res.getRequest().getCheckInDate()) >= 0 &&
                             request.getCheckInDate().isBefore(res.getRequest().getCheckOutDate())) ||
-                    (request.getCheckOutDate().compareTo(res.getRequest().getCheckInDate()) >= 0 &&
-                            request.getCheckOutDate().compareTo(res.getRequest().getCheckOutDate()) <= 0))) {
+                            (request.getCheckOutDate().compareTo(res.getRequest().getCheckInDate()) >= 0 &&
+                                    request.getCheckOutDate().compareTo(res.getRequest().getCheckOutDate()) <= 0))) {
                 wrongRooms.add(room);
                 break;
             }
         }
     }
 
+    /**
+     * Save reservations
+     *
+     * @param reservationDto the reservation dto
+     * @param request        the request
+     * @param bindingResult  the binding result
+     * @param model          the model
+     * @return the page to redirect to
+     */
     @PostMapping("/add-reservation")
     public String addReservation(@ModelAttribute("reservation") ReservationDto reservationDto,
                                  @ModelAttribute("request") Request request,
@@ -146,6 +218,12 @@ public class AdminController {
         return "redirect:/admin/requests";
     }
 
+    /**
+     * Added attributed and gets the page with the room form.
+     *
+     * @param model the model
+     * @return the page
+     */
     @GetMapping("/add-room")
     public String getRoomForm(Model model) {
         model.addAttribute("error", null);
@@ -154,6 +232,14 @@ public class AdminController {
         return "admin/add-room-form";
     }
 
+    /**
+     * Add new room.
+     *
+     * @param roomDto       the room dto
+     * @param bindingResult the binding result
+     * @param model         the model
+     * @return the page
+     */
     @PostMapping("/add-room")
     public String addNewRoom(@ModelAttribute("newroom") RoomDto roomDto,
                              BindingResult bindingResult, Model model) {
@@ -173,7 +259,13 @@ public class AdminController {
         }
     }
 
-    //TODO: almost same method is in ClientController
+    /**
+     * Gets reservation info.
+     *
+     * @param requestId the request id
+     * @param model     the model
+     * @return 5the page
+     */
     @GetMapping("check-reservation/{id}")
     public String getReservationInfo(@PathVariable("id") Long requestId,
                                      Model model) {
@@ -191,6 +283,11 @@ public class AdminController {
         }
     }
 
+    /**
+     * Gets index page.
+     *
+     * @return the index page
+     */
     @GetMapping("/index")
     public String getIndexPage() {
         return "index";
--- src/main/java/ua/polina/hotel_reservation/service/RequestService.java
+++ src/main/java/ua/polina/hotel_reservation/service/RequestService.java
@@ -14,15 +14,36 @@ import ua.polina.hotel_reservation.repository.RequestRepository;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * The type Request service.
+ *
+ * @author Polina Serhiienko
+ */
 @Service
 public class RequestService {
+    /**
+     * The Request repository.
+     */
     RequestRepository requestRepository;
 
+    /**
+     * Class constructor, instantiates a new Request service.
+     *
+     * @param requestRepository the request repository
+     */
     @Autowired
     public RequestService(RequestRepository requestRepository) {
         this.requestRepository = requestRepository;
     }
 
+    /**
+     * Save new request.
+     *
+     * @param requestDto  the request dto
+     * @param client      the client
+     * @param description the description of the room
+     * @return the request
+     */
     public Request saveNewRequest(RequestDto requestDto, Client client, Description description) {
         Request request = Request.builder()
                 .client(client)
@@ -34,19 +55,44 @@ public class RequestService {
         return  requestRepository.save(request);
     }
 
+    /**
+     * Get all requests with pagination.
+     *
+     * @param pageable the pageable
+     * @return the page
+     */
     public Page<Request> getAllRequests(Pageable pageable){
         return requestRepository.findAll(pageable);
     }
 
+    /**
+     * Get request by id.
+     *
+     * @param id the id
+     * @return the optional of request
+     */
     public Optional<Request> getRequestById(Long id){
         return requestRepository.findById(id);
     }
 
+    /**
+     * Update request.
+     *
+     * @param request the request
+     * @param status  new status of request
+     * @return the request
+     */
     public Request update(Request request, Status status){
         request.setStatus(status);
         return requestRepository.save(request);
     }
 
+    /**
+     * Get requests by client.
+     *
+     * @param client the client
+     * @return the request list
+     */
     public List<Request> getRequestsByClient(Client client){
         return requestRepository.findRequestByClient(client);
     }
